192c71bba57fe03524de7387f29c0f659336183e
zaliauskastempe <46976027+zaliauskastempe@users.noreply.github.com>
2019-01-26 13:26:47 -0500

    Add files via upload

---
+++ cameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour
+{
+    public GameObject player; //brings the player object into the script so we can reference it
+    private Vector3 offset; //makes a new variable 
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;  //Use transform. to get the physical position, rotation, scale etc of an element.
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
