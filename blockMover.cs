e9257e05d27cfb20242e5825379cd29dafa5f931
zaliauskastempe <46976027+zaliauskastempe@users.noreply.github.com>
2019-02-03 21:46:40 -0500

    Add files via upload

---
+++ blockMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockMover : MonoBehaviour
+{
+    public float zSpot;
+    private bool direction;
+    public GameObject MovingWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        zSpot = transform.position.z;
+        direction = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == true)  
+        {
+            Vector3 temp = new Vector3(0, 0, 0.05f);
+            MovingWall.transform.position -= temp;
+        }
+        else
+        {
+            Vector3 temp = new Vector3(0, 0, 0.05f);
+            MovingWall.transform.position += temp;
+        }
+        if (Mathf.Abs(transform.position.z) >= zSpot) direction = !direction;
+    }
+}
--- playerController.cs
+++ playerController.cs
@@ -6,44 +6,85 @@ public class playerController : MonoBehaviour
 {
 
     public float speed;
+    public int room;
     public Text countText;
+    public Text lifeText;
     public Text winText;
+    public Text scoreText;
     private Rigidbody rb;
     private int count;
+    private int lives;
+    private int score;
+    public GameObject Player;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        room = 1;
+        lives = 3;
         SetCountText();
         winText.text = "";
         SetCountText();
     }
 
-    void FixedUpdate()
+    void FixedUpdate() //Move the player around
     {
+        if (lives < 1)
+        {
+            enabled = false;
+            winText.text = "Game Over!  You ran out of lives!";
+        }
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) //when we touch the collectables
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            SetCountText();
+            other.gameObject.SetActive(false); //turn them off
+            score = score + 1;
+            count = count + 1; //increase our count
+            SetCountText(); //update the score text
+        }
+        if (other.gameObject.CompareTag("Red Pick Up"))
+        {
+            other.gameObject.SetActive(false); //turn them off
+            lives = lives - 1; //increase our count
+            score = score - 1; 
+            SetCountText(); //update the lives text
         }
     }
 
     void SetCountText()
     {
+        lifeText.text = "Lives: " + lives.ToString();
+        scoreText.text = "Score: " + score.ToString();
         countText.text = "Count: " + count.ToString();
         if (count >= 12)
         {
-            winText.text = "You Win!";
+            if (room == 2)
+            {
+                countText.fontStyle = FontStyle.Bold;
+                winText.text = "You Win!";
+            }
+
+            if (room == 1)
+            {
+                transform.position = new Vector3(38.21f, 1, -5.59f);   //GO TO NEXT LEVEL
+                count = 0;
+                lives = 3;
+                room = 2;
+                lifeText.text = "Lives: " + lives.ToString();
+                scoreText.text = "Score: " + score.ToString();
+                countText.text = "Count: " + count.ToString();
+            }
+
         }
     }
+
 }
